34e2deae5b169c15fd25a7415db6720fc0006ee9
Sam <thesamreaves@gmail.com>
2019-02-02 21:14:04 -0500

    Successfully downloading photo from Photo object url, displaying on screen using task on the main thread

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2019 Sam Reaves Digital. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success([Photo])
@@ -29,6 +38,22 @@ class PhotoStore {
         return FlickrAPI.photos(fromJSON: jsonData)
     }
     
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData)
+            else {
+                if (data == nil) {
+                    return .failure(error!)
+                }
+                else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        
+        return .success(image)
+    }
+    
     func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
@@ -36,7 +61,25 @@ class PhotoStore {
             (data, response, error) -> Void in
             
                 let result = self.processPhotosRequest(data: data, error: error)
-                completion(result)
+                OperationQueue.main.addOperation {
+                    completion(result)
+                }
+        }
+        task.resume()
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error ) -> Void in
+            
+                let result = self.processImageRequest(data: data, error: error)
+                OperationQueue.main.addOperation {
+                    completion(result)
+                }
+            
         }
         task.resume()
     }
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -23,11 +23,26 @@ class PhotosViewController: UIViewController {
                 
                 case let .success(photos):
                     print("Successfully found \(photos.count) photos")
+                    if let firstPhoto = photos.first {
+                        self.updateImageView(for: firstPhoto)
+                    }
                 
                 case let .failure(error):
                     print("Error fetching photos: \(error)")
                 }
             
+        }
+    }
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) in
+            switch imageResult {
+                case let .success(image):
+                    self.imageView.image = image
+                case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
         }
     }
 }
